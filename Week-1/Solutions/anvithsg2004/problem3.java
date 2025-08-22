85207b6072f8edd04160927b69039ed98e8408e3
Anvith S G <anvithsg2004@gmail.com>
2025-08-22 21:03:06 +0530

    feat(week-1): Add solution for question 3

--- Week-1/Solutions/anvithsg2004/problem3.java
+++ Week-1/Solutions/anvithsg2004/problem3.java
@@ -1,2 +1,28 @@
+import java.util.HashSet;
+
 public class problem3 {
+
+    public Character problem(String s) {
+
+        int n = s.length();
+
+        HashSet<Character> hashSet = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+
+            if (!hashSet.isEmpty() && hashSet.contains(s.charAt(i))) {
+                return s.charAt(i);
+            }
+
+            hashSet.add(s.charAt(i));
+
+        }
+
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
