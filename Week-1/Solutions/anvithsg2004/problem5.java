8501720ce1a1968f824b86162ffd01650562afe1
Anvith S G <anvithsg2004@gmail.com>
2025-08-22 21:20:02 +0530

    feat(week-1): Add solution for question 5

--- Week-1/Solutions/anvithsg2004/problem5.java
+++ Week-1/Solutions/anvithsg2004/problem5.java
@@ -1,2 +1,14 @@
 public class problem5 {
+
+    public static int problem(int n) {
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        return problem(n - 1) + problem(n - 2);
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
