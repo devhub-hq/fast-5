97d1a9b6b47866d819bcdf4eb41f584adc976215
Anvith S G <anvithsg2004@gmail.com>
2025-08-22 20:45:32 +0530

    feat(week-1): Add solution for question 1

---
+++ Week-1/Solutions/anvithsg2004/problem1.java
@@ -0,0 +1,20 @@
+public class problem1 {
+
+    public int problem1(int[] arr, int n) {
+
+        int givenSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            givenSum = givenSum + arr[i];
+        }
+
+        int realSum = (n * (n + 1)) / 2;
+
+        return realSum - givenSum;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
