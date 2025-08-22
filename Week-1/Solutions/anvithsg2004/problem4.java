c2cf4e9207dfbf7d75aa2cfb35b91e4e549e316a
Anvith S G <anvithsg2004@gmail.com>
2025-08-22 21:18:06 +0530

    feat(week-1): Add solution for question 4

--- Week-1/Solutions/anvithsg2004/problem4.java
+++ Week-1/Solutions/anvithsg2004/problem4.java
@@ -1,2 +1,28 @@
 public class problem4 {
+
+    public String problem(int n, int x, int[] arr) {
+        return help(arr, 0, 0, x, 0) ? "Yes" : "No";
+    }
+
+    public boolean help(int[] arr, int index, int count, int x, int sum) {
+
+        if (count == x) {
+            return (sum % 2) != 0; // directly check when picked X snacks
+        }
+
+        if (index >= arr.length) {
+            return false; // no more snacks to pick
+        }
+
+        // choose or skip
+        boolean take = help(arr, index + 1, count + 1, x, sum + arr[index]);
+        boolean notTake = help(arr, index + 1, count, x, sum);
+
+        return take || notTake;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
