3fd1de8f61ddedd6dbdb407287fa8a40ea346677
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2025-08-26 12:17:12 +0530

    [fix[ o(1) fro q4

--- Week-2/Solutions/4.non_ajacent.py
+++ Week-2/Solutions/4.non_ajacent.py
@@ -1,13 +1,9 @@
-def sol(arr:list[int])->int:
-    n=len(arr)
-    if n==0:return 0
-    if n==1:return arr[0]
-    dp=[0]*n
-    dp[0]=arr[0]
-    dp[1]=max(arr[0],arr[1])
-    for i in range(2,n):
-        dp[i]=max(dp[i-1],dp[i-2]+arr[i])
-    return dp[-1]
+def sol(arr: list[int]) -> int:
+    take=skip = 0
+    for x in arr:
+        take,skip=skip+x,max(skip,take)
+    return max(take,skip)
+
 
 
 assert sol([5,5,10,100,10,5])==110
