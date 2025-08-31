bf2bb95f77170895ca9eb94fbdde9674c3d801cd
Tsaishashanth <saishashanthtarigopula@gmail.com>
2025-08-31 20:25:02 +0530

    added solution for evenpair problem

---
+++ Week2/Solutions/SaishashanthTarigopula/NonAdjacent.java
@@ -0,0 +1,15 @@
+// package Week2.Solutions.SaishashanthTarigopula;
+
+// public class NonAdjacent {
+//     public int nonAdjacent(int[] arr) {
+//         //here we should not care about the value but we have to care about the indexs,no adjacent index should should be counted 
+//         //we have the return the maximum value at final
+//         //even odd sumation and maximum wont work in this case
+//         // int[] arr = {5, 5, 10, 100, 10, 5} = 110;
+
+//         for(int i=0; i< arr.length; i++) {
+//             //so we have to add the alterante elements and then 
+//         }
+        
+//     }
+// }
