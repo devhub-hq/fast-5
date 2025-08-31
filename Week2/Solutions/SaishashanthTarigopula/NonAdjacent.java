e7ab118e7b9be906dd9c31835e6236230c136f6d
Tsaishashanth <saishashanthtarigopula@gmail.com>
2025-09-01 00:19:49 +0530

    added solution for NonAdjacent problem

--- Week2/Solutions/SaishashanthTarigopula/NonAdjacent.java
+++ Week2/Solutions/SaishashanthTarigopula/NonAdjacent.java
@@ -1,15 +1,37 @@
-// package Week2.Solutions.SaishashanthTarigopula;
+package Week2.Solutions.SaishashanthTarigopula;
 
-// public class NonAdjacent {
-//     public int nonAdjacent(int[] arr) {
-//         //here we should not care about the value but we have to care about the indexs,no adjacent index should should be counted 
-//         //we have the return the maximum value at final
-//         //even odd sumation and maximum wont work in this case
-//         // int[] arr = {5, 5, 10, 100, 10, 5} = 110;
 
-//         for(int i=0; i< arr.length; i++) {
-//             //so we have to add the alterante elements and then 
-//         }
+class NonAdjacent {
+    public int maxSum(int[] arr) {
+        int n = arr.length;
+
+
+        if (n == 0) return 0;
+
+
+        if (n == 1) return arr[0];
+
+
+        int[] store = new int[n];
+
         
-//     }
-// }
+        store[0] = arr[0];                        
+        store[1] = Math.max(arr[0], arr[1]);      
+
+
+        for (int i = 2; i < n; i++) {
+
+            store[i] = Math.max(arr[i] + store[i - 2], store[i - 1]);
+        }
+
+
+        return store[n - 1];
+    }
+
+    public static void main(String[] args) {
+        NonAdjacent obj = new NonAdjacent();
+        int[] arr = {5, 5, 10, 100, 10, 5};
+
+        System.out.println(obj.maxSum(arr));  // Output → 110
+    }
+}
